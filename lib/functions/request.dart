acd4500f5b66d61f0774ffa6840f258e64fc655c
3to1null <nathanklumpenaar10@outlook.com>
2018-09-11 12:56:10 +0200

    Get requests working & tested, build get param string from map.

--- lib/functions/request.dart
+++ lib/functions/request.dart
@@ -1,10 +1,32 @@
-import 'package:flutter/services.dart';
 import 'package:http/http.dart';
+import 'dart:async';
 
-const String baseURL= "http://127.0.0.1::8000/ichthus";
+const String baseURL= "http://192.168.2.7:8000/ichthus";
+
+String transformMapToString(Map data){
+  String dataString = "?";
+  data.forEach((k, v){
+    k.toString();
+    v.toString();
+    dataString += '$k=$v&';
+  });
+  return dataString;
+}
 
 getDataFromAPI(String path, [Map data]) async {
   String requestURL = baseURL + path;
-  final test = await get(requestURL);
-  print(test);
+  if(data != null){
+    String dataString = transformMapToString(data);
+    requestURL += dataString;
+  }
+  final response = await get(requestURL);
+  return(response.body);
+  //return response;
 }
+
+postDataToAPI(String path, Map data) async {
+  String requestURL = baseURL + path;
+  final test = await post(requestURL, body: data);
+  print(test.body);
+}
+
