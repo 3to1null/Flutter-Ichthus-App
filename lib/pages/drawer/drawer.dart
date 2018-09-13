888235fce37e361beb38209fc21d6c72085a533f
3to1null <nathanklumpenaar10@outlook.com>
2018-09-13 12:14:12 +0200

    Added a drawer that display's account info.

---
+++ lib/pages/drawer/drawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart';
+
+import 'get_link_profile_picture.dart';
+
+import 'drawer_data_model.dart';
+import '../../models/user_model.dart';
+
+class CompleteDrawer extends StatefulWidget {
+  @override
+  _CompleteDrawerState createState() => _CompleteDrawerState();
+}
+
+class _CompleteDrawerState extends State<CompleteDrawer> {
+
+  bool hasProfilePictureLink = false;
+  Map profilePictureHeaders;
+  String profilePictureUrl;
+
+  DrawerDataModel drawerDataModel = DrawerDataModel();
+
+  void updateStateToLoadProfilePicture(Map headers, String url){
+
+    drawerDataModel.hasProfilePictureLink = true;
+    drawerDataModel.profilePictureUrl = url;
+    drawerDataModel.profilePictureHeaders = headers;
+    setState(() {
+          hasProfilePictureLink = true;
+          profilePictureUrl = url;
+          profilePictureHeaders = headers;
+        });
+    
+  }
+
+  UserModel userModel = UserModel();
+
+  @override
+  Widget build(BuildContext context) {
+
+    hasProfilePictureLink = drawerDataModel.hasProfilePictureLink;
+    profilePictureHeaders = drawerDataModel.profilePictureHeaders;
+    profilePictureUrl = drawerDataModel.profilePictureUrl;
+    
+    if(!hasProfilePictureLink){
+      getLinkToProfilePicture(updateStateToLoadProfilePicture);
+    }
+    
+
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text(userModel.userName),
+            accountEmail: Text("${userModel.userGroup} | ${userModel.userCode}"),
+            currentAccountPicture: hasProfilePictureLink ? CircleAvatar(
+               backgroundImage: AdvancedNetworkImage(
+                 profilePictureUrl,
+                 header: {'Cookie': profilePictureHeaders["Cookie"]},
+                 useDiskCache: false,
+                 loadFailedCallback: (){
+                   drawerDataModel.hasProfilePictureLink = false;
+                   setState(() {
+                      hasProfilePictureLink = false;                    
+                  });
+                 }
+               ),
+           ): CircleAvatar(
+             child: Text("NK"),
+           ),
+
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/drawer/drawer_data_model.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+class DrawerDataModel {
+  static final DrawerDataModel _drawerDataModel = new DrawerDataModel._internal();
+
+  factory DrawerDataModel() {
+    return _drawerDataModel;
+  }
+
+  DrawerDataModel._internal();
+
+  bool hasProfilePictureLink = false;
+  Map profilePictureHeaders;
+  String profilePictureUrl;
+
+}
