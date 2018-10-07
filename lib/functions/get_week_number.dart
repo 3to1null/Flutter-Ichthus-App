772ee205b1086d09a1fdf8233bb496c52e642469
3to1null <nathanklumpenaar10@outlook.com>
2018-10-07 17:31:45 +0200

    Hotfixed the weeknumber

--- lib/functions/get_week_number.dart
+++ lib/functions/get_week_number.dart
@@ -1,9 +1,10 @@
+//TODO: Probably need to change the math in this function.
 int getWeekNumber() {
   final date = DateTime.now();
   final startOfYear = new DateTime(date.year, 1, 1, 0, 0);
   final firstMonday = startOfYear.weekday;
   final daysInFirstWeek = 8 - firstMonday;
   final diff = date.difference(startOfYear);
-  var weeks = ((diff.inDays - daysInFirstWeek) / 7).ceil();
+  var weeks = ((diff.inDays - daysInFirstWeek) / 7).ceil() + 1;
   return weeks;
 }
