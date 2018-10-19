3586a438b5bb45f3f5ac8aef04c2fbf2f9b7c981
3to1null <nathanklumpenaar10@outlook.com>
2018-10-19 22:40:53 +0200

    Now displaying average + subject in expansionTile

--- lib/pages/cijfer_page/functions/get_cijfers.dart
+++ lib/pages/cijfer_page/functions/get_cijfers.dart
@@ -6,12 +6,10 @@ import '../../../functions/request.dart';
 
 import '../../../models/user_model.dart';
 
-
-
 Future<bool> _hasThisCijferPeriodInCache(int period) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String userMarks = prefs.getString("_userMarksPeriod$period");
-  if(userMarks == null){
+  if (userMarks == null) {
     return false;
   }
   return true;
@@ -19,12 +17,14 @@ Future<bool> _hasThisCijferPeriodInCache(int period) async {
 
 Stream<List<Map<String, dynamic>>> getCijfers(int period) async* {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  if(await _hasThisCijferPeriodInCache(period)){
+  if (await _hasThisCijferPeriodInCache(period)) {
     String userMarks = prefs.getString("_userMarksPeriod$period");
     yield List<Map<String, dynamic>>.from(json.decode(userMarks));
   }
   String cijfersResponse =
       await postDataToAPI('/marks/get', {"periode": period.toString()});
-  yield List<Map<String, dynamic>>.from(json.decode(cijfersResponse));
-  prefs.setString("_userMarksPeriod$period", cijfersResponse);
+  if (cijfersResponse != null) {
+    yield List<Map<String, dynamic>>.from(json.decode(cijfersResponse));
+    prefs.setString("_userMarksPeriod$period", cijfersResponse);
+  }
 }
--- lib/pages/cijfer_page/widgets/cijfer_list_widget.dart
+++ lib/pages/cijfer_page/widgets/cijfer_list_widget.dart
@@ -7,13 +7,41 @@ class CijferList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return ListView.builder(
       itemCount: cijferListData.length,
       padding: EdgeInsets.all(8.0),
       //itemExtent: 20.0,
-      itemBuilder: (BuildContext context, int index){
-        return Text(cijferListData[index].toString());
+      itemBuilder: (BuildContext context, int index) {
+
+        Color circleColor(Map<String, dynamic> cijferData){
+          if(cijferData["average"] == '-'){
+            return Colors.green;
+          }else{
+            num average = num.parse(cijferData["average"]);
+            if(average >= 5.5){
+              return Colors.green;
+            }else{
+              return Colors.red;
+            }
+          }
+        }
+
+        Map<String, dynamic> cijferData = cijferListData[index];
+        return ExpansionTile(
+          leading: CircleAvatar(
+              backgroundColor: circleColor(cijferData),
+              child: Text(
+                cijferData["average"],
+                style: Theme.of(context)
+                    .textTheme
+                    .body2
+                    .copyWith(color: Colors.white),
+              )),
+          title: Text(cijferData['subject']),
+          children: <Widget>[],
+        );
       },
-    ); 
+    );
   }
 }
