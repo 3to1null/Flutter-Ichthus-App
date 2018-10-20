668df9dac1f8a4bf329cc21df9986e5e5c90b766
3to1null <nathanklumpenaar10@outlook.com>
2018-10-20 15:36:08 +0200

    Stop spamming server for new marks

--- lib/pages/cijfer_page/functions/get_cijfers.dart
+++ lib/pages/cijfer_page/functions/get_cijfers.dart
@@ -4,7 +4,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../functions/request.dart';
 
+import '../models/cijfer_data_model.dart';
 import '../../../models/user_model.dart';
+
+CijferDataModel _cijferDataModel = CijferDataModel();
 
 Future<bool> _hasThisCijferPeriodInCache(int period) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -16,15 +19,21 @@ Future<bool> _hasThisCijferPeriodInCache(int period) async {
 }
 
 Stream<List<Map<String, dynamic>>> getCijfers(int period) async* {
+  if(_cijferDataModel.periodsLoadedThisRun == null){
+    _cijferDataModel.periodsLoadedThisRun = [0];
+  }
   SharedPreferences prefs = await SharedPreferences.getInstance();
   if (await _hasThisCijferPeriodInCache(period)) {
     String userMarks = prefs.getString("_userMarksPeriod$period");
     yield List<Map<String, dynamic>>.from(json.decode(userMarks));
   }
-  String cijfersResponse =
-      await postDataToAPI('/marks/get', {"periode": period.toString()});
-  if (cijfersResponse != null) {
-    yield List<Map<String, dynamic>>.from(json.decode(cijfersResponse));
-    prefs.setString("_userMarksPeriod$period", cijfersResponse);
+  if (!_cijferDataModel.periodsLoadedThisRun.contains(period)) {
+    String cijfersResponse =
+        await postDataToAPI('/marks/get', {"periode": period.toString()});
+    if (cijfersResponse != null) {
+      _cijferDataModel.periodsLoadedThisRun.add(period);
+      yield List<Map<String, dynamic>>.from(json.decode(cijfersResponse));
+      prefs.setString("_userMarksPeriod$period", cijfersResponse);
+    }
   }
 }
