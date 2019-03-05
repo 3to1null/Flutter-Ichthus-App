c9d2a4957d70ff55ce08a3d2a4b86d6650f34e98
3to1null <nathanklumpenaar10@outlook.com>
2019-03-05 16:08:19 +0100

    Added security info

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -10,6 +10,35 @@ import '../../widgets/loading_animation.dart';
 
 import '../../functions/convert_to_bool.dart';
 
+
+
+const String sec_msg_short = 
+"""
+Zodra jij inlogt wordt je wachtwoord heel even gebruikt om een zermelo-koppelingscode op te halen. Deze is nodig voor het verkrijgen van je rooster. Je wachtwoord word daarna ge-encrypt opgeslagen, de sleutel wordt verwijderd van de server en naar je mobiel gestuurd. 
+Dit betekent dat zowel de server als je telefoon (en dus elke app op je telefoon) niet bij je wachtwoord kan, behalve als jij, via deze app, een verzoek stuurt naar de server. Dit doe je bijvoorbeeld als je je cijfers laadt.
+""";
+
+const String sec_msg_long_1 =
+"""
+Wanneer er wordt ingelogd wordt je wachtwoord gebruikt om je identiteit te controleren door middel van een login op de Zermelo API. Als de combinatie van je wachtwoord en gebruikersnaam correct is wordt er een uniek ID gegenereerd. Dit ID word gebruikt om je identieit te confirmeren met de server in latere requests. Je wachtwoord wordt na het inloggen ge-encrypt op de server opgeslagen. Hiervoor wordt het AES-256 algoritme gebruikt. De key wordt daarna naar je mobiel gestuurd en wordt NOOIT opgeslagen op de server.
+""";
+
+const String sec_msg_long_2 =
+"""
+Het gevolg hiervan is dat zowel de server als de client (deze app/je telefoon) niet zonder elkaar bij je wachtwoord kunnnen. Dit betekent dat voor een aantal requests, bijvoorbeeld voor je cijfers, bestanden en andere informatie van leerlingweb, de key vanaf je mobiel naar de server wordt gestuurd. De server gebruikt de key om je wachtwoord kort te decrypten en zo in te loggen op de service waar het de data vandaan haalt. Het gedecrypte wachtwoord wordt niet opgeslagen.
+""";
+
+const String sec_msg_long_3 = 
+"""
+Je gegevens worden gehaald vanaf verschillende punten; Zermelo API, Leerlingweb, SOM-implementatie op Leerlingweb, ItsLearning en IchthusDrive. Voor alle datapunten, met uitzondering van de Zermelo API, is je wachtwoord elke keer opnieuw nodig. Dit is de reden dat je wachtwoord niet gehasht maar ge-encrypt wordt.
+Al deze gegevens worden verzameld en opgeslagen op de server, om vanaf daar naar je mobiel verzonden te worden. Dit is helaas nodig omdat het ophalen van sommige data langer dan 5 minuten kan duren. De server zorgt ervoor dat dit wordt gedaan voordat jij het nodig hebt, zodat wanneer jij het nodig hebt de server alleen de gecachde data hoeft te versturen. 
+""";
+
+const String sec_msg_long_4 = 
+"""
+Verder wordt praktisch alle data, behalve natuurlijk je wachtwoord, op je mobiel opgeslagen. Dit gebeurd op een plek waar, op niet geunlockte telefoons, alleen deze app bij kan. Dit wordt gedaan zodat je ook je roosters en cijfers kunt bekijken zonder een internetverbinding. Ook wordt de lokale data gebruikt door algorithmes die proberen een goede balans te vinden tussen snelheid en recentheid van data bij langzame verbindingen.
+""";
+
 class _LoginData {
   String leerlingnummer = '';
   String password = '';
@@ -95,7 +124,8 @@ class _ActualLoginPageState extends State<ActualLoginPage> {
       }
     } catch (e) {
       _loginPageScaffoldKey.currentState.showSnackBar(SnackBar(
-        content: Text("Er is iets fout gegaan. Controleer je gegevens en internetverbinding en probeer het opnieuw."),
+        content: Text(
+            "Er is iets fout gegaan. Controleer je gegevens en internetverbinding en probeer het opnieuw."),
       ));
       setState(() {
         isLoading = false;
@@ -198,8 +228,7 @@ class _UserLoginInputState extends State<UserLoginInput> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        //padding: EdgeInsets.symmetric(),
-        child: ListView(
+    child: ListView(
       children: <Widget>[
         Container(
             padding: EdgeInsets.symmetric(horizontal: 30.0),
@@ -228,28 +257,53 @@ class _UserLoginInputState extends State<UserLoginInput> {
               ],
             )),
         Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0),
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.lock, color: Theme.of(context).primaryColor),
-                Spacer(),
-                Expanded(
-                  flex: 10,
-                  child: TextField(
-                    obscureText: true,
-                    maxLines: 1,
-                    controller: loginPasswordController,
-                    style: Theme.of(context)
-                        .textTheme
-                        .body1
-                        .copyWith(fontSize: 16.0),
-                    decoration: InputDecoration(
-                        labelText: "Wachtwoord",
-                        contentPadding: EdgeInsets.all(2.5)),
-                  ),
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.lock, color: Theme.of(context).primaryColor),
+              Spacer(),
+              Expanded(
+                flex: 10,
+                child: TextField(
+                  obscureText: true,
+                  maxLines: 1,
+                  controller: loginPasswordController,
+                  style: Theme.of(context)
+                      .textTheme
+                      .body1
+                      .copyWith(fontSize: 16.0),
+                  decoration: InputDecoration(
+                      labelText: "Wachtwoord",
+                      contentPadding: EdgeInsets.all(2.5)),
                 ),
-              ],
-            )),
+              ),
+            ],
+          )
+        ),
+        Padding(child: Divider(), padding: EdgeInsets.only(top: 16.0)),
+        FlatButton(
+          child: Align(child: Text("Lees meer over de veiligheid van deze app en je gegevens...", style: Theme.of(context).textTheme.caption.copyWith(fontSize: 12.0)), alignment: Alignment.centerLeft),
+          onPressed: (){
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context){
+                return ListView(
+                  padding: EdgeInsets.all(16.0),
+                  children: <Widget>[
+                    Text("In het kort", style: Theme.of(context).textTheme.body2),
+                    Text(sec_msg_short),
+                    Divider(),
+                    Text("De wat langere uitleg", style: Theme.of(context).textTheme.body2),
+                    Text(sec_msg_long_1),
+                    Text(sec_msg_long_2),
+                    Text(sec_msg_long_3),
+                    Text(sec_msg_long_4),
+                  ],
+                );
+              }
+            );
+          },
+        )
       ],
     ));
   }
