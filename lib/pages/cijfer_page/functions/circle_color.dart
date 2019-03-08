6f5dccb85247c4ecd481eab76c020718e64741f0
3to1null <nathanklumpenaar10@outlook.com>
2019-03-08 18:05:09 +0100

    Fixed bug in markspage

--- lib/pages/cijfer_page/functions/circle_color.dart
+++ lib/pages/cijfer_page/functions/circle_color.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 Color circleColor(dynamic mark) {
-  if (mark == '-' || mark == '*') {
+  if (mark == '-' || mark == '*' || mark == 'V' || mark == 'G') {
     return Colors.green;
+  } else if (mark == 'O') {
+    return Colors.red;
   } else {
-    num parsedMark = num.parse(mark);
-    if (parsedMark >= 5.5) {
+    try{
+      num parsedMark = num.parse(mark);
+      if (parsedMark >= 5.5) {
+        return Colors.green;
+      } else {
+        return Colors.red;
+      }
+    }catch(FormatException){
       return Colors.green;
-    } else {
-      return Colors.red;
     }
   }
 }
