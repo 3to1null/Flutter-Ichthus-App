eac5d44f096e814f3b1d8d07475c04fa801e8267
3to1null <nathanklumpenaar10@outlook.com>
2019-07-03 15:11:31 +0200

    Added error loggin to create new fodler

--- lib/pages/files_page/widgets/new_folder_dialog.dart
+++ lib/pages/files_page/widgets/new_folder_dialog.dart
@@ -8,12 +8,40 @@ class NewFolderDialog extends StatefulWidget {
   _NewFolderDialogState createState() => _NewFolderDialogState();
 
   final BuildContext pageContext;
-  final Folder folder;
-  NewFolderDialog(this.pageContext, this.folder);
+  final Folder parentFolder;
+  NewFolderDialog(this.pageContext, this.parentFolder);
 }
 
 class _NewFolderDialogState extends State<NewFolderDialog> {
   TextEditingController _folderNameTextController = TextEditingController();
+  String errorMessage;
+  List<String> folderNames;
+  bool hasTypedFirstChar = false;
+
+  @override
+  void initState() {
+    folderNames = widget.parentFolder.cachedChildFolders.map((f) => f.name).toList();
+    _folderNameTextController.addListener((){
+      String currentValue = _folderNameTextController.text;
+      if(currentValue.length > 0){
+        hasTypedFirstChar = true;
+      }
+      if(folderNames.contains(currentValue)){
+        setState(() {
+         errorMessage = "Deze naam is al in gebruik."; 
+        });
+      }else if(currentValue == "" && hasTypedFirstChar){
+        setState(() {
+         errorMessage = "Vul een naam in voor de map."; 
+        });
+      }else{
+        setState(() {
+         errorMessage = null; 
+        });
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +57,7 @@ class _NewFolderDialogState extends State<NewFolderDialog> {
             textCapitalization: TextCapitalization.words,
             style: Theme.of(context).textTheme.body1.copyWith(fontSize: 16.0),
             decoration: InputDecoration(
+              errorText: errorMessage,
               contentPadding: EdgeInsets.fromLTRB(4.0, 16.0, 4.0, 8.0),
               fillColor: Colors.black12,
               filled: true,
@@ -38,14 +67,10 @@ class _NewFolderDialogState extends State<NewFolderDialog> {
             FlatButton(
               child: new Text('Opslaan'),
               textColor: Theme.of(context).primaryColor,
-              onPressed: () {
-                if(_folderNameTextController.text != ""){
-                  // TODO: Check if name already exists!!
-                  createNewFolder(widget.folder, _folderNameTextController.text);
-                  Navigator.of(context).pop();
-                  Navigator.of(widget.pageContext).pop();
-                }
-                // TODO: Error logging to user!!
+              onPressed: errorMessage != null ? null : () {
+                createNewFolder(widget.parentFolder, _folderNameTextController.text);
+                Navigator.of(context).pop();
+                Navigator.of(widget.pageContext).pop();
               },
             )
           ],
